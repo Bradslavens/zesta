4105a831ad7e54a8459b41b1776880d5b483b9cf
Brad Slavens <bradslavens@gmail.com>
2019-01-25 17:53:29 +0000

    added ajax form handler

--- js/custom.js
+++ js/custom.js
@@ -11,4 +11,18 @@ $(function()
         $("html").scrollTop(sp + 165);
         $(".getStarted").toggle(500);
     })
+    
+    $("#submitAddressToZillow").submit(function( event ){
+        event.preventDefault();
+        console.log("form submitted");
+        
+        var address = $("input[name=address").val();
+        var citystatezip = $("input[name=citystatezip").val();
+        console.log(address + citystatezip);
+        
+        $.get("getDetails.php", {address: address, citystatezip: citystatezip})
+        .done(function( data ){
+            console.log(data);
+        });
+    })
 });
