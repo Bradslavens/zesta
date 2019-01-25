8f16c5ef9e48227396efaab7836f5106756797b5
Brad Slavens <bradslavens@gmail.com>
2019-01-25 16:45:05 +0000

    completed landing page basic layout with form to request zestimate

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function()
+{
+    $("#howItWorks").on("click", function(){
+        $(".howItWorks").toggle(500);
+    });
+    
+    $("#getStarted").on("click", function(){
+        $(".getStarted").toggle(500);
+    })
+});
