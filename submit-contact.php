30a44f3f2f3fd8ad0c78be9f3b68d3990c14c352
Brad Slavens <bradslavens@gmail.com>
2019-01-26 02:56:32 +0000

    added submit contact script to add new contact to db

--- submit-contact.php
+++ submit-contact.php
@@ -1,3 +1,38 @@
 <?php
-echo $_POST['name']. PHP_EOL;
-echo $_POST['email']. PHP_EOL;
+
+$path = dirname(__FILE__);
+
+require_once($path . '/vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+$mysqli = new mysqli(getenv("db_host"),getenv("db_username"),getenv("db_password"),"users");
+
+if(mysqli_connect_errno()){
+    printf("connection failed: %s\n", mysqli_connect_errno());
+    exit();
+};
+
+// __________  sanitize form input __________
+//
+$name  = addslashes($mysqli->real_escape_string($_POST['name']));
+$name = filter_var($name, FILTER_SANITIZE_STRING);
+$email = addslashes($mysqli->real_escape_string($_POST['email']));
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+// _________________  prepare statement ___________________
+// 
+if(!($statement = $mysqli->prepare("INSERT INTO users1(name, email) VALUES (?,?)"))){
+    echo "Prepair failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+// ___________________  bind and execute _______________________
+//
+if(!($statement->bind_param("ss", $name, $email ))) {
+    echo "Binding parameters failed: " . $statement->errno . " " . $statement->error;
+}
+
+if(!($statement->execute())){
+    echo "Statement failed to execute: " . $statement->errno . " " . $statement->error;
+}
